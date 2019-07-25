a7f32c1cdcd1b5d9caa77288e512de23e352bdc1
mohit <kaushikmohit54@hotmail.com>
2019-07-25 12:10:40 +0530

    first commit

---
+++ test-output/report.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:Features/Login.feature");
+formatter.feature({
+  "name": "Login",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "Successful Login with Valid Credentials",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@login"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User Launch Chrome browser",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "Steps.user_Launch_Chrome_browser()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User opens URL \"http://admin-demo.nopcommerce.com/login\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Steps.user_opens_URL(String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "User enters Email as \"admdin@yourstore.com\" and Password as \"admin\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.user_enters_Email_as_and_Password_as(String,String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on Login",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.click_on_Login()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Page Title should be \"Dashboard / nopCommerce administration\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Steps.page_Title_should_be(String)"
+});
+formatter.result({
+  "error_message": "java.lang.AssertionError\r\n\tat org.junit.Assert.fail(Assert.java:86)\r\n\tat org.junit.Assert.assertTrue(Assert.java:41)\r\n\tat org.junit.Assert.assertTrue(Assert.java:52)\r\n\tat stepDefinitions.Steps.page_Title_should_be(Steps.java:91)\r\n\tat ✽.Page Title should be \"Dashboard / nopCommerce administration\"(file:Features/Login.feature:9)\r\n",
+  "status": "failed"
+});
+formatter.step({
+  "name": "User click on Log out link",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Steps.user_click_on_Log_out_link()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "Page Title should be \"Your store. Login\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Steps.page_Title_should_be(String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.step({
+  "name": "close browser",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.close_browser()"
+});
+formatter.result({
+  "status": "skipped"
+});
+});
